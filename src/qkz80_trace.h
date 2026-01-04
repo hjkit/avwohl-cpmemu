573bf660327dbe91853d4da05c0646ef39e4dddd
Aaron Wohl <wohl@awohl.com>
2026-01-03 19:27:39 -0500

    supress trace unused warnings

--- src/qkz80_trace.h
+++ src/qkz80_trace.h
@@ -4,21 +4,27 @@
 class qkz80_trace {
  public:
   virtual void comment(const char *fmt,...) {
+    (void)fmt;
   }
 
   virtual void asm_op(const char *fmt,...) {
+    (void)fmt;
   }
   
   virtual void flush(void) {
   }
 
   virtual void fetch(qkz80_uint8 opstream_byte,qkz80_uint16 pc) {
+    (void)opstream_byte;
+    (void)pc;
   }
 
   virtual void add_reg8(qkz80_uint8 areg) {
+    (void)areg;
   }
 
   virtual void add_reg16(qkz80_uint16 areg) {
+    (void)areg;
   }
   
 };
